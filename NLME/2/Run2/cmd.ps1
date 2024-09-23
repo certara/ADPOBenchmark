23f8f49e8cfc1c62374178f6eb1954002bcc7483
certara-msale <102747664+certara-msale@users.noreply.github.com>
2024-09-23 11:52:38 -0700

    With NLME models folder

---
+++ NLME/2/Run2/cmd.ps1
@@ -0,0 +1,8 @@
+\n$env:NLME_HASH = 1770978959
+$INSTALLDIR="D:\NLME_Engine_ADPO"
+if($INSTALLDIR -eq "" -or $INSTALLDIR -eq $null)
+{
+  "Installation directory is not specified"
+  exit 1
+}
+powershell -noninteractive -executionpolicy remotesigned -File $INSTALLDIR\generic_run.ps1 none $INSTALLDIR $shared_directory D:\git\ADPOBenchmark\NLME\2\Run2 D:\git\ADPOBenchmark\NLME\2\Run2\jobControlFile.txt 1 WorkFlow
