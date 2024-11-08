b3122b360f6c138c5c8256308b4e658000c49566
certara-jcraig <61294078+certara-jcraig@users.noreply.github.com>
2024-11-07 20:21:46 -0800

    clean files

--- NLME/2/Run2/cmd.ps1
+++
@@ -1,8 +0,0 @@
-\n$env:NLME_HASH = 1770978959
-$INSTALLDIR="C:\Program Files\Certara\NLME_Engine"
-if($INSTALLDIR -eq "" -or $INSTALLDIR -eq $null)
-{
-  "Installation directory is not specified"
-  exit 1
-}
-powershell -noninteractive -executionpolicy remotesigned -File $INSTALLDIR\generic_run.ps1 none $INSTALLDIR $shared_directory D:\git\ADPOBenchmark\NLME\2\Run2 D:\git\ADPOBenchmark\NLME\2\Run2\jobControlFile.txt 1 WorkFlow
--- NLME/35/Run35/cmd.ps1
+++
@@ -1,8 +0,0 @@
-\n$env:NLME_HASH = 1770978959
-$INSTALLDIR="D:\NLME_Engine_noHessian"
-if($INSTALLDIR -eq "" -or $INSTALLDIR -eq $null)
-{
-  "Installation directory is not specified"
-  exit 1
-}
-powershell -noninteractive -executionpolicy remotesigned -File $INSTALLDIR\generic_run.ps1 none $INSTALLDIR $shared_directory D:\git\ADPOBenchmark\NLME\35\Run35 D:\git\ADPOBenchmark\NLME\35\Run35\jobControlFile.txt 1 WorkFlow
